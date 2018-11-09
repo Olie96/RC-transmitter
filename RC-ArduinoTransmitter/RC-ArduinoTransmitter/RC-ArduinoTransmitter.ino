11718d9d322960dfa4134c23db9e5f6c95e74b16
Oliver Brežnjak <breza.oliver@gmail.com>
2018-11-09 21:31:36 +0100

    beginig

--- RC-ArduinoTransmitter/RC-ArduinoTransmitter/RC-ArduinoTransmitter.ino
+++ RC-ArduinoTransmitter/RC-ArduinoTransmitter/RC-ArduinoTransmitter.ino
@@ -1,45 +1,66 @@
 #include <UTFT.h>
 #include <UTouch.h>
 
+#define LOW_PRIORITY 200
+#define MEDIUM_PRIORITY 100
+
 UTFT Screen(SSD1963_800480, 38, 39, 40, 41);  
 UTouch  myTouch(43, 42, 44, 45, 46); 
 extern uint8_t BigFont[];
 
 int x, y;
+int lowPriority, mediumPriority;
+// config
+void SetUpPinModes();
 void SetUpLcdConfig();
 void LoadOperatingSystem();
-void SetMainMenu();
-String CalcutateBatteryPercentage();
-void SetUpPinModes();
+void InitializeMainMenu();
+// header
+void InitializeHeader();
+void ShowBatteryPercentage();
+void ShowGpsStrength();
+void ShowVehicleStatus();
+
+// footer
+void InitializeFooter();
 
 void setup()
 {
+	SetUpPinModes();
 	SetUpLcdConfig();
 	LoadOperatingSystem();
-	SetMainMenu();
+	InitializeMainMenu();
 }
 
 void loop()
 {
-	//while (true)
-	//{
-	//	if (myTouch.dataAvailable())
-	//	{
-	//		myTouch.read();
-	//		x = myTouch.getX();
-	//		y = myTouch.getY();
-	//
-	//		if ((y >= 10) && (y <= 60))  // Upper row
-	//		{
-	//			if ((x >= 10) && (x <= 60))  // Button: 1
-	//			{
-	//				//waitForIt(10, 10, 60, 60);
-	//				//updateStr('1');
-	//			}	//
-	//			
-	//		}
-	//	}
-	//}
+	if (mediumPriority > MEDIUM_PRIORITY)
+	{
+		mediumPriority = 0;
+	}
+	if (lowPriority > LOW_PRIORITY)
+	{
+		ShowGpsStrength();
+		ShowBatteryPercentage();
+		ShowVehicleStatus();
+		lowPriority = 0;
+	}
+
+	mediumPriority++;
+	lowPriority++;
+}
+
+
+void LoadOperatingSystem()
+{
+	Screen.print("Loading operating system", 210, 215);
+	Screen.setColor(255, 255, 255);
+	for (x = 10; x <= 790; x++)
+	{
+		Screen.fillRoundRect(10, 235, x, 245);
+	}
+	delay(500);
+	Screen.clrScr();
 }
 
 void SetUpPinModes()
@@ -48,7 +69,6 @@ void SetUpPinModes()
 	digitalWrite(8, HIGH);
 	pinMode(A0, INPUT);
 }
-
 
 void SetUpLcdConfig()
 {
@@ -59,40 +79,54 @@ void SetUpLcdConfig()
 	Screen.setFont(BigFont);
 }
 
-void LoadOperatingSystem()
+void InitializeMainMenu()
 {
-	Screen.print("Loading operating system", 210, 215);
-	Screen.setColor(0, 0, 255);
-	for (x = 10; x <= 790; x++)
-	{
-		Screen.fillRoundRect(10, 235, x, 245);
-		//Screen.setColor(255, 255, 255);
-	}
-	delay(500);
-	Screen.clrScr();
-	//	Screen.print("0", 267, 87);
-	//	Screen.setColor(0, 0, 255);
-	//	Screen.fillRoundRect(10, 130, 150, 180);
-	//	Screen.setColor(255, 255, 255);
-	//	Screen.drawRoundRect(10, 130, 150, 180);
-	//	Screen.print("Clear", 40, 147);
-	//	Screen.setColor(0, 0, 255);
-	//	Screen.fillRoundRect(160, 130, 300, 180);
-	//	Screen.setColor(255, 255, 255);
-	//	Screen.drawRoundRect(160, 130, 300, 180);
-	//	Screen.print("Enter", 190, 147);
-	//	Screen.setBackColor(0, 0, 0);
+	InitializeHeader();
+	InitializeFooter();
+	ShowGpsStrength();
+	ShowBatteryPercentage();
+	ShowVehicleStatus();
 }
 
-void SetMainMenu()
-{
-	//Screen.print(CalcutateBatteryPercentage() + " V", 650, 1);
+// header
 
+void InitializeHeader()
+{
+	Screen.setColor(0, 10, 255);
+	Screen.setBackColor(0, 10, 255);
+	Screen.fillRoundRect(0, 0, 799, 18);
+	Screen.setColor(255, 255, 255);
 }
 
-String CalcutateBatteryPercentage()
+void ShowBatteryPercentage()
 {
 	int batteryRead = analogRead(A0);
+	batteryRead = map(batteryRead, 800,1023,0,100);
+	Screen.print(String(batteryRead) + "%", 700,1);
+	Screen.setColor(0, 200, 0);
+	Screen.fillRoundRect(755, 4, 770, 12);
+	Screen.fillRoundRect(760, 1, 798, 15);
+	Screen.setColor(255, 255, 255);
+}
 
-	return String(batteryRead);
+void ShowGpsStrength()
+{
+	Screen.print("GPS", 580, 1);
+	Screen.print("68%", 635, 1);
+}
+
+void ShowVehicleStatus()
+{
+	Screen.print("Status:", 10, 1);
+	Screen.print("Ready", 125, 1);
+}
+
+// footer
+
+void InitializeFooter()
+{
+	Screen.setColor(0, 10, 255);
+	Screen.setBackColor(0, 10, 255);
+	Screen.fillRoundRect(0, 462, 800, 480);
+	Screen.setColor(255, 255, 255);
 }
